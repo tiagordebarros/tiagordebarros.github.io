21da2e3a4bf16a9fc683976faa69ff75011c26c4
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2022-09-09 00:13:15 -0300

    Realizadas pequenas alterações antes do deploy

--- portfolio-web/src/components/header/CTA.jsx
+++ portfolio-web/src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ function CTA() {
     return(
         <div className="cta">
             <a href={ CV } download className="btn">Download CV</a>
-            <a href="#contact" className="btn btn-primary">Let's Talk</a>
+            <a href="mailto:tiagordebarros@gmail.com?subject=Contato Portfólio Web" className="btn btn-primary">Let's Talk</a>
         </div>
     );
 }
--- portfolio-web/src/components/header/HeaderSocials.jsx
+++ portfolio-web/src/components/header/HeaderSocials.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-import { BsYoutube } from 'react-icons/bs';
+// import { BsYoutube } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md'
 
 
 function HeaderSocials() {
@@ -9,7 +10,8 @@ function HeaderSocials() {
         <div className="header__socials">
             <a href="https://www.linkedin.com/in/tiagoribeirodebarros/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
             <a href="https://github.com/tiagordebarros" target="_blank" rel="noreferrer"><FaGithub /></a>
-            <a href="https://www.youtube.com/channel/UCaA8zVzGntGTrYCL7_0EpiA" target="_blank" rel="noreferrer"><BsYoutube /></a>
+            {/* <a href="https://www.youtube.com/channel/UCaA8zVzGntGTrYCL7_0EpiA" target="_blank" rel="noreferrer"><BsYoutube /></a> */}
+            <a href="mailto:tiagordebarros@gmail.com?subject=Contato Portfólio Web" target="_blank" rel="noreferrer"><MdEmail /></a>
         </div>
     );
 }
